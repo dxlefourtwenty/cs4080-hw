172da37088b125812b9ef410f0db78afeb7f5832
Dale Peligro <ddparmstrong2021@gmail.com>
2026-02-16 21:57:29 -0800

    added question2.java

---
+++ 3-hw3/ch7/question2.java
@@ -0,0 +1,13 @@
+private String stringify(Object object) {
+  if (object == null) return "nil";
+
+  if (object instanceof Double) {
+    String text = object.toString();
+    if (text.endsWith(".0")) {
+      text = text.substring(0, text.length() - 2);
+    }
+    return text;
+  }
+
+  return object.toString();
+}
